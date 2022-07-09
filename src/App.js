840a942854d333cee0567ac3d884723c7ece52db
oluwajueda <oluwajuedaloade@gmail.com>
2022-07-09 05:13:52 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./Navbar";
 import Service from "./Service";
 import Portfolio from "./Portfolio";
 import Footer from "./Footer";
+import Introduction from "./Introduction";
 
 
 function App() {
   return <div>
     
      <Navbar/>
+   <Introduction/>
      <Service/>
       <Portfolio/>
       <Footer/>
--- src/Service.js
+++ src/Service.js
@@ -5,10 +5,12 @@ const Service = () => {
     <div className='first-div'>
     <h3>SERVICES</h3>
     <h2>What I AM Great At</h2>
+    <div className='p-div'>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi neque risus, facilisis at elementum non,
          vulputate sed lorem. Mauris vel turpis maximus, porttitor dui in,
          vehicula diam. Morbi vel dolor id nunc fringilla maximus. 
          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+         </div>
       </div>  
       <div className='box-container'>
       <div className='service-box'><h3>Web Development</h3></div>
